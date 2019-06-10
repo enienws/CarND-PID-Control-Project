74d68262bf7eb3374d339844342fca5ba79ed5d4
Engin FIRAT <engin.firat@adonissyazilim.com>
2019-06-10 07:39:33 +0300

    commit before test
    Signed-off-by: Engin FIRAT <engin.firat@adonissyazilim.com>

--- src/PID.cpp
+++ src/PID.cpp
@@ -12,19 +12,27 @@ void PID::Init(double Kp_, double Ki_, double Kd_) {
   /**
    * TODO: Initialize PID coefficients (and errors, if needed)
    */
+	this->Kp = Kp_;
+	this->Ki = Ki_;
+	this->Kd = Kd_;
 
+	p_error = i_error = d_error = 0.0;
 }
 
 void PID::UpdateError(double cte) {
   /**
    * TODO: Update PID errors based on cte.
    */
-
+	p_error = cte;
+	d_error = cte - d_error ;
+	i_error += cte;
 }
 
 double PID::TotalError() {
   /**
    * TODO: Calculate and return the total error
    */
-  return 0.0;  // TODO: Add your total error calc here!
+	double error =
+			-p_error * Kp - d_error * Kd - i_error * Ki;
+  return error;
 }
